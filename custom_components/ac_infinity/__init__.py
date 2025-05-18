877275e7264abc04425263f635b862289bb0f3fd
dalinicus <contact@dalinicus.com>
2025-05-17 23:02:38 -0500

    properly close clients when installing, configuring, or uninstalling

--- custom_components/ac_infinity/__init__.py
+++ custom_components/ac_infinity/__init__.py
@@ -44,7 +44,7 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         coordinator = hass.data[DOMAIN][entry.entry_id]
-        await coordinator.close()
+        await coordinator.ac_infinity.close()
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
